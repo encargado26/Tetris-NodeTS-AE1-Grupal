2ed988d3f51d3148ab614f2a7db845461bcc7784
encargado26 <caceresparedmarceloagustin@gmail.com>
2026-08-26 13:55:50 -0300

    implemente class board

---
+++ src/Board.ts
@@ -0,0 +1,14 @@
+export class Cell {}
+
+export class Board {
+    public width: number;
+    public height: number; 
+    public cells: Cell[][]; 
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.cells = Array.from({ length: height }, () => Array.from({ length: width }, () => new Cell())); 
+        
+    }
+}
