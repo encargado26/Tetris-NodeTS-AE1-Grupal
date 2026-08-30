080468ea87e261498810b242c458bd173f793d0b
German.R <german.05rodriguez@gmail.com>
2026-08-30 15:39:09 -0300

    creo PieceStick

---
+++ src/PieceStick.ts
@@ -0,0 +1,18 @@
+import { _PiezaBase } from "./PieceBase";
+
+export class _PiezaStick extends _PiezaBase {
+
+  constructor(posX: number = 0, posY: number = 0) {
+
+    super(
+      [
+        [1],
+        [1],
+        [1],
+        [1]
+      ],
+      posX,
+      posY
+    );
+  }
+}
